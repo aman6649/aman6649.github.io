5c210f71acccde9eb16465036ffb94a900b94874
Gupta <amangup@apac.corpdir.net>
2023-10-17 10:09:47 +0530

    caching whole site

--- sw_cached_sites.js
+++ sw_cached_sites.js
@@ -1,22 +1,11 @@
-const cacheName = 'v1';
+const cacheName = 'v2';
 
-const cacheAssets=[
-  'index.html',
-  'styles.css',
-  'main.js'
-]
+
 
 
 self.addEventListener('install', e =>{
   console.log('service installed');
-  e.waitUntil(
-    caches.open(cacheName)
-    .then(cache => {
-      console.log("service worker: cacheing files");
-      cache.addAll(cacheAssets)
-    })
-    .then(()=> self.skipWaiting())
-  )
+  
 })
 
 self.addEventListener('activate', e =>{
@@ -38,5 +27,15 @@ self.addEventListener('activate', e =>{
 
 self.addEventListener('fetch', e => {
   console.log('service worker: fetching');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+  e.respondWith(
+    fetch(e.request)
+    .then(res => {
+      const resClone = res.clone();
+      caches.open(cacheName)
+      .then(cache => {
+        cache.put(e.request,resClone)
+      })
+      return res;
+    }).catch(err =>  caches.match(e.request).then(res => res))
+  )
 })
